6e319da37e179d91b71d3460d5823a34300298b0
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-08-31 19:07:36 +0530

    Add TARA ChromaDB vector store code

---
+++ src/07_chroma_vector_store.py
@@ -0,0 +1,16 @@
+chroma_client = chromadb.PersistentClient(
+    path="./chroma_db"
+)
+collection = chroma_client.create_collection(name="my_collections")
+
+ids = []
+for i in range(len(chunks)):
+    ids.append(str(i))
+
+collection.add(
+    ids=ids,
+    documents=chunks,
+    embeddings=embeddings.tolist()
+)
+
+print(collection.count())

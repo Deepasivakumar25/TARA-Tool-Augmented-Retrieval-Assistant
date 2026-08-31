b5eca5526fc60774f65d770ab95b2132b91dd5f1
Deepasivakumar <103418256+Deepasivakumar25@users.noreply.github.com>
2026-08-31 18:56:30 +0530

    Add TARA imports module

---
+++ src/02_imports.py
@@ -0,0 +1,7 @@
+from transformers import pipeline
+from sentence_transformers import SentenceTransformer
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+import chromadb
+import pypdf
+from pypdf import PdfReader
+from transformers import AutoModelForCausalLM, AutoTokenizer, pipeline
